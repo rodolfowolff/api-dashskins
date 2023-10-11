b371005317f598f788618b67c250e54c2bbd2d88
rodolfowolff <rodolfoluizwolff@gmail.com>
2023-10-11 00:15:13 -0300

    feat: validar dados user

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,46 @@
-export class CreateUserDto {}
+import {
+  IsNotEmpty,
+  IsEmail,
+  IsString,
+  Length,
+  IsNumber,
+  Min,
+  Max,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @IsNotEmpty({ message: 'Nome é obrigatório.' })
+  @IsString({ each: true, message: 'Nome deve ser um texto.' })
+  @Length(5, 255, {
+    message: 'O nome deve ter entre 5 e 255 caracteres.',
+  })
+  readonly username: string;
+
+  /**  */
+  @IsNotEmpty({ message: 'Email é obrigatório.' })
+  @IsString({ each: true, message: 'Email deve ser um texto.' })
+  @IsEmail({}, { message: 'Email inválido.' })
+  readonly email: string;
+
+  /**  */
+  @IsNotEmpty({ message: 'idade é obrigatório.' })
+  @IsNumber(
+    { allowInfinity: false, allowNaN: false },
+    { message: 'Idade deve ser um número válido.' },
+  )
+  @Min(1, {
+    message: 'Idade deve ser no mínimo 1.',
+  })
+  @Max(110, {
+    message: 'Idade deve ser no máximo 110.',
+  })
+  readonly age: number;
+
+  /**  */
+  @IsNotEmpty({ message: 'Avatar é obrigatório.' })
+  @IsString({ each: true, message: 'Avatar deve ser um texto.' })
+  @Length(1, 255, {
+    message: 'Avatar deve ter entre 1 e 255 caracteres.',
+  })
+  readonly avatar: string;
+}
