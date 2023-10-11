6c63ca29e57bf766f22f32963fade1d2807dcccf
rodolfowolff <rodolfoluizwolff@gmail.com>
2023-10-11 01:16:27 -0300

    feat: metodo privado verificar email unico

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,9 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
+import { UserRepository } from './repository/user.repository';
 
 @Injectable()
 export class UsersService {
-  create(body: CreateUserDto) {
+  constructor(private readonly userRepository: UserRepository) { }
+  async create(body: CreateUserDto) {
+    await this.isEmailUnique(body.email);
     return body;
+  }
+
+  /** Metodos privados */
+  private async isEmailUnique(email: string) {
+    const emailExists = await this.userRepository.findByCondition(
+      { email },
+      { email: 1 }, // retorna somente email
+    );
+    if (emailExists) {
+      throw new BadRequestException('Email em uso.');
+    }
   }
 }
