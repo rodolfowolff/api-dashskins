36e15f9c6363579cedfb64666d79eee38f7946aa
rodolfowolff <rodolfoluizwolff@gmail.com>
2023-10-10 21:59:14 -0300

    feat: configura endpoint e porta

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const globalPrefix = 'api';
+  app.setGlobalPrefix(globalPrefix);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  Logger.log(`🚀 Api on: http://localhost:${port}/${globalPrefix}`);
 }
 bootstrap();
