9f9c7e4fe4a28ecd73d6a117e11643a8a64f7de0
rodolfowolff <rodolfoluizwolff@gmail.com>
2023-10-10 23:28:03 -0300

    feat: gerado user crud

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot(process.env.MONGO_URI || 'mongodb://localhost/nest'),
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
