2759e4e2472ec4808e11ec754f008e5fea70b55b
rodolfowolff <rodolfoluizwolff@gmail.com>
2023-10-16 13:54:36 -0300

    feat: habilitar cors

--- src/main.ts
+++ src/main.ts
@@ -2,19 +2,44 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  // const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const envConfig = app.get(ConfigService);
 
+  const whitelist = [
+    'https://dashskin.vercel.app',
+    'https://www.dashskin.vercel.app',
+    'https://dashskin.vercel.app/',
+    'https://www.dashskin.vercel.app/',
+    'dashskin.vercel.app',
+  ];
   app.enableCors({
-    allowedHeaders: '*',
-    origin: '*', // [envConfig.get<string>('base_url.frontend')],
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    origin: function (origin, callback) {
+      if (whitelist.indexOf(origin) !== -1) {
+        console.log('allowed cors for:', origin);
+        callback(null, true);
+      } else {
+        console.log('blocked cors for:', origin);
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    allowedHeaders:
+      'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
+    methods: 'GET,PUT,POST,DELETE,UPDATE,OPTIONS',
     credentials: true,
   });
 
-  app.useGlobalPipes(new ValidationPipe());
+  // app.enableCors({
+  //   allowedHeaders: '*',
+  //   origin: 'http://localhost:3000', // [envConfig.get<string>('base_url.frontend')],
+  //   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  //   credentials: true,
+  // });
+
+  // app.useGlobalPipes(new ValidationPipe());
 
   // const globalPrefix = 'api';
   // app.setGlobalPrefix(globalPrefix);
