57b4fdd768d2e6b829f4811dde56714b397875eb
rodolfowolff <rodolfoluizwolff@gmail.com>
2023-10-16 02:12:57 -0300

    temp: remove global prefix: api

--- src/main.ts
+++ src/main.ts
@@ -7,10 +7,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const envConfig = app.get(ConfigService);
 
-  // app.enableCors({
-  //   origin: [envConfig.get<string>('base_url.frontend')],
-  //   credentials: true,
-  // })
+  app.enableCors({
+    origin: '*',
+    // origin: [envConfig.get<string>('base_url.frontend')],
+    // credentials: true,
+  });
   app.useGlobalPipes(new ValidationPipe());
 
   // const globalPrefix = 'api';
