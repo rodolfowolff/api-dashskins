c7e172c137a046a6572f2b0440776c9d55c95d1d
rodolfowolff <rodolfoluizwolff@gmail.com>
2023-10-16 15:35:34 -0300

    feat: removido tentativas de configuracoes de bloqueio de cors

--- src/main.ts
+++ src/main.ts
@@ -1,46 +1,34 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  // const app = await NestFactory.create(AppModule);
-  const app = await NestFactory.create(AppModule, {
-    cors: true,
-  });
+  const app = await NestFactory.create(AppModule);
   const envConfig = app.get(ConfigService);
 
-  // const whitelist = [
-  //   'https://dashskin.vercel.app',
-  //   'https://www.dashskin.vercel.app',
-  //   'https://dashskin.vercel.app/',
-  //   'https://www.dashskin.vercel.app/',
-  //   'dashskin.vercel.app',
-  // ];
-  // app.enableCors({
-  //   origin: function (origin, callback) {
-  //     if (whitelist.indexOf(origin) !== -1) {
-  //       console.log('allowed cors for:', origin);
-  //       callback(null, true);
-  //     } else {
-  //       console.log('blocked cors for:', origin);
-  //       callback(new Error('Not allowed by CORS'));
-  //     }
-  //   },
-  //   allowedHeaders:
-  //     'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
-  //   methods: 'GET,PUT,POST,DELETE,UPDATE,OPTIONS',
-  //   credentials: true,
-  // });
+  const whitelist = [
+    'https://dashskin.vercel.app',
+    'https://www.dashskin.vercel.app',
+    'https://dashskin.vercel.app/',
+    'https://www.dashskin.vercel.app/',
+    'dashskin.vercel.app',
+  ];
+  app.enableCors({
+    origin: function (origin, callback) {
+      if (whitelist.indexOf(origin) !== -1) {
+        console.log('allowed cors for:', origin);
+        callback(null, true);
+      } else {
+        console.log('blocked cors for:', origin);
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    methods: 'GET,PUT,POST,DELETE,UPDATE,OPTIONS',
+    credentials: true,
+  });
 
-  // app.enableCors({
-  //   allowedHeaders: '*',
-  //   origin: 'http://localhost:3000', // [envConfig.get<string>('base_url.frontend')],
-  //   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  //   credentials: true,
-  // });
-
-  // app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe());
 
   // const globalPrefix = 'api';
   // app.setGlobalPrefix(globalPrefix);
