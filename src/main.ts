914c4397bc0a24fa18fb374960f9de8f81339ad4
rodolfowolff <rodolfoluizwolff@gmail.com>
2023-10-16 13:24:42 -0300

    feat: habilitar cors

--- src/main.ts
+++ src/main.ts
@@ -7,11 +7,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const envConfig = app.get(ConfigService);
 
-  // app.enableCors({
-  // origin: '*',
-  // origin: [envConfig.get<string>('base_url.frontend')],
-  // credentials: true,
-  // });
+  app.enableCors({
+    // origin: [envConfig.get<string>('base_url.frontend')],
+    origin: '*',
+    credentials: true,
+  });
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
 
